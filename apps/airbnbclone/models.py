d24a72f80381c03d76d4779d342224c3395f9c18
rwjlee <lee.rwj@gmail.com>
2018-05-04 17:23:49 +0800

    update model

--- apps/airbnbclone/models.py
+++ apps/airbnbclone/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=256)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Favorite(models.Model):
+    
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
