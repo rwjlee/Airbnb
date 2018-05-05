64ce1535ab69abec528fbcbf5fec7894350f48a8
rwjlee <lee.rwj@gmail.com>
2018-05-05 14:19:29 +0800

    basic model and navbar

--- apps/airbnbclone/models.py
+++ apps/airbnbclone/models.py
@@ -10,8 +10,38 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Favorite(models.Model):
-    
-    
+class Amenity(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Listing(models.Model):
+
+    host = models.ForeignKey(User, related_name = 'has_listings', on_delete=models.CASCADE)
+    desc = models.TextField(blank=True)
+    bedroom = models.IntegerField()
+    bath = models.IntegerField()
+    bed = models.IntegerField()
+    num_guests = models.IntegerField()
+
+    address = models.TextField(blank=True)
+    
+    amenities = models.ManyToManyField(Amenity, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Booking(models.Model):
+    guest = models.ForeignKey(User, related_name = 'has_bookings', on_delete=models.CASCADE)
+    home_listing = models.ForeignKey(Listing, related_name='has_guests', on_delete=models.CASCADE)
+    charge_amount = models.FloatField()
+    
+    from_date = models.DateField()
+    to_date = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
