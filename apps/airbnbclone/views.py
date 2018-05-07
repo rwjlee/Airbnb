926a3d34cf706e850a6e8b021b8ebbcbcedc36dd
ET+ALEX CryptoWork <CryptoWork@Mais-MacBook-Pro.local>
2018-05-07 11:15:59 +0800

    added map

--- apps/airbnbclone/views.py
+++ apps/airbnbclone/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render, redirect
+import googlemaps
+from datetime import datetime
+from pprint import pprint
+from apps.airbnbclone.constants import MAP_API_KEY
 
 # Create your views here.
 def index(request):
     return render(request, 'airbnbclone/index.html')
+
+def map(request):
+    query = request.GET["html_term".replace(" ", "+")]
+    
+    context = {
+        'api_key' : MAP_API_KEY,
+        'query' : query
+    }
+    return render(request, 'airbnbclone/map.html', context)
+
+
+    
